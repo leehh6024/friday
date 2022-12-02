b747969820cf2a4ad96497468d9a2d9b6aeaf14d
leehh6024 <dlgusghchlrh@gmail.com>
2022-12-02 13:27:27 +0900

    .

--- app/pages/Home/Footer.js
+++ app/pages/Home/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Image } from "react-native";
+// import { Image } from "react-native";
 
 export default function Footer() {
   return (
     <View style={styles.container}>
-      <Image source={require("~/assets/images/briefingBtn.png")} />
+      {/* <Image source={require("~/assets/images/briefingBtn.png")} /> */}
     </View>
   );
 }
