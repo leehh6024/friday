e67540a9e425f733db8fb0a7c49f93d4b493cdd0
leehh6024 <dlgusghchlrh@gmail.com>
2022-12-04 15:54:22 +0900

    modify: Settings, Guide Page

--- app/pages/Guide/Guide.js
+++ app/pages/Guide/Guide.js
@@ -1,21 +1,67 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function Guide() {
   const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text>Guide Page</Text>
-      <Button title="go to Home" onPress={() => navigation.navigate("Home")} />
+      <View style={styles.header}>
+        <Text style={styles.title}>GUIDE</Text>
+        <Ionicons
+          name="ios-home-outline"
+          style={styles.home}
+          onPress={() => navigation.navigate("Home")}
+        />
+      </View>
+      <View style={styles.body}></View>
+      <View style={styles.footer}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 44,
-    alignItems: "center",
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    flex: 0.3,
+    backgroundColor: "#B9CFDF",
+    // color: "#A3C1C6",
+    color: "white",
+    width: SCREEN_WIDTH,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingHorizontal: 10,
+  },
+  body: {
+    flex: 2.0,
+    backgroundColor: "#A3C1C6",
+    width: SCREEN_WIDTH,
+  },
+  footer: {
+    flex: 0.3,
+    backgroundColor: "#B9CFDF",
+    width: SCREEN_WIDTH,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "800",
+    color: "#A3C1C6",
+    marginTop: 44,
+    marginRight: 70,
+  },
+  home: {
+    marginTop: 44,
+    fontSize: 30,
+    fontWeight: "800",
+    color: "#e5e5e5",
   },
 });
