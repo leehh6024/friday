bf3ff7ea3464cd051d5b739d6c9f50bd431ff577
leehh6024 <dlgusghchlrh@gmail.com>
2022-12-07 17:03:12 +0900

    Create: Modal of Settings

--- app/pages/Settings/Settings.js
+++ app/pages/Settings/Settings.js
@@ -1,14 +1,22 @@
-import React from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Modal,
+  Button,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons, AntDesign } from "@expo/vector-icons";
 import BottomBar from "../Home/components/BottomBar.js";
-// import Keyword from "./components/Keyword.js";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function Settings() {
   const navigation = useNavigation();
+  const [showModalKeyword, setShowModalKeyword] = useState(false);
+  const [showModalTime, setShowModalTime] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -23,12 +31,65 @@ export default function Settings() {
       <View style={styles.body}>
         <View style={styles.flexbox}>
           <Text style={styles.function}>{`키워드 설정`}</Text>
-          <AntDesign name="rightcircleo" size={24} color="#A3C1C6" />
-          {/* <Keyword /> */}
+          <AntDesign
+            name="rightcircleo"
+            size={24}
+            color="#A3C1C6"
+            onPress={() => {
+              setShowModalKeyword(!showModalKeyword);
+            }}
+          />
         </View>
         <View style={{ ...styles.flexbox, marginTop: 6 }}>
           <Text style={styles.function}>{`센서 작동 시간 설정`}</Text>
-          <AntDesign name="rightcircleo" size={24} color="#A3C1C6" />
+          <AntDesign
+            name="rightcircleo"
+            size={24}
+            color="#A3C1C6"
+            onPress={() => {
+              setShowModalTime(!showModalTime);
+            }}
+          />
+        </View>
+        <View style={styles.keyword}>
+          <Modal
+            animationType={"slide"}
+            transparent={false}
+            visible={showModalKeyword}
+            onRequestClose={() => {
+              console.log("Modal has been closed.");
+            }}
+          >
+            <View style={styles.modal}>
+              <Text style={styles.text}>키워드 설정 모달</Text>
+              <Button
+                title="Click To Close Modal"
+                onPress={() => {
+                  setShowModalKeyword(!showModalKeyword);
+                }}
+              />
+            </View>
+          </Modal>
+        </View>
+        <View style={styles.timeSet}>
+          <Modal
+            animationType={"slide"}
+            transparent={false}
+            visible={showModalTime}
+            onRequestClose={() => {
+              console.log("Modal has been closed.");
+            }}
+          >
+            <View style={styles.modal}>
+              <Text style={styles.text}>센서 작동 시간 설정 모달</Text>
+              <Button
+                title="Click To Close Modal"
+                onPress={() => {
+                  setShowModalTime(!showModalTime);
+                }}
+              />
+            </View>
+          </Modal>
         </View>
       </View>
       <View style={styles.footer}>
@@ -92,5 +153,11 @@ const styles = StyleSheet.create({
     color: "#A3C1C6",
     fontWeight: "800",
     fontSize: 24,
+  },
+  modal: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#00ff00",
+    padding: 100,
   },
 });
