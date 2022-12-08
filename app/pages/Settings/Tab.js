793030f18f7528f8be0fcbe2ebd4773472f9b4fc
leehh6024 <dlgusghchlrh@gmail.com>
2022-12-08 16:50:02 +0900

    Create: Palette of Modal Tab

--- app/pages/Settings/Tab.js
+++ app/pages/Settings/Tab.js
@@ -5,28 +5,28 @@ export default function Tab({ activeTab, setActiveTab }) {
   return (
     <View>
       <Text
-        style={styles.categoryText}
+        style={styles(activeTab).categoryText1}
         isActive={activeTab === "C"}
         onPress={() => setActiveTab("C")}
       >
         일정
       </Text>
       <Text
-        style={styles.categoryText}
+        style={styles(activeTab).categoryText2}
         isActive={activeTab === "W"}
         onPress={() => setActiveTab("W")}
       >
         날씨
       </Text>
       <Text
-        style={styles.categoryText}
+        style={styles(activeTab).categoryText3}
         isActive={activeTab === "B"}
         onPress={() => setActiveTab("B")}
       >
         버스
       </Text>
       <Text
-        style={styles.categoryText}
+        style={styles(activeTab).categoryText4}
         isActive={activeTab === "N"}
         onPress={() => setActiveTab("N")}
       >
@@ -36,11 +36,34 @@ export default function Tab({ activeTab, setActiveTab }) {
   );
 }
 
-const styles = StyleSheet.create({
-  categoryText: {
-    marginBottom: 30,
-    fontSize: 18,
-    color: "#666666",
-    fontWeight: "700",
-  },
-});
+const styles = (activeTab) =>
+  StyleSheet.create({
+    categoryText1: {
+      marginBottom: 30,
+      fontSize: 18,
+      color: "#666666",
+      fontWeight: "700",
+      backgroundColor: activeTab === "C" ? "blue" : null,
+    },
+    categoryText2: {
+      marginBottom: 30,
+      fontSize: 18,
+      color: "#666666",
+      fontWeight: "700",
+      backgroundColor: activeTab === "W" ? "blue" : null,
+    },
+    categoryText3: {
+      marginBottom: 30,
+      fontSize: 18,
+      color: "#666666",
+      fontWeight: "700",
+      backgroundColor: activeTab === "B" ? "blue" : null,
+    },
+    categoryText4: {
+      marginBottom: 30,
+      fontSize: 18,
+      color: "#666666",
+      fontWeight: "700",
+      backgroundColor: activeTab === "N" ? "blue" : null,
+    },
+  });
