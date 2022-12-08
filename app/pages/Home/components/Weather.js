7579431afb7bfc152511373323ae386bdf05a0b5
leehh6024 <dlgusghchlrh@gmail.com>
2022-12-08 19:22:46 +0900

    Modify: Weather API

--- app/pages/Home/components/Weather.js
+++ app/pages/Home/components/Weather.js
@@ -1,18 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import * as Location from "expo-location";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  ActivityIndicator,
-} from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { Fontisto, Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-
-const API_KEY = "ef3118cc42b4ccfbf7cc900504e6b835";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const icons = {
   Clouds: "cloudy",
@@ -23,55 +15,33 @@ const icons = {
   Drizzle: "rain",
   Thunderstorm: "lighting",
 };
+const BASE_IP = "http://172.16.239.139:8080";
 
 export default function Weather() {
   const [city, setCity] = useState("Loading...");
+  const [loading, setLoading] = useState(false);
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-
   const navigation = useNavigation();
 
+  const appId = useRef("");
+
   const weatherAPI = async () => {
+    console.log("getWeather: appId: ", appId);
     const res = await axios.get(
-      "http://172.16.239.139:8080/weather/getWeather?appId=1234"
+      `${BASE_IP}/weather/getWeather?appId=${appId.current}`
     );
-    console.log(JSON.parse(res.data.res).daily);
-    console.log(typeof JSON.parse(res.data.res));
+    const json = await JSON.parse(res.data.weatherInfo);
+    setDays(json.daily);
+    // console.log(json.daily);
+    // console.log(typeof json.daily);
+    // console.log(res.data.city);
+    setCity(res.data.city);
   };
-  weatherAPI();
-  // const getCoordinate = async () => {
-  //   const res = await getWeatherAPI();
-  //   console.log(res.data);
-  // };
 
-  // useEffect(() => {
-  //   // AsyncStorage.clear(); // 사용금지
-  //   idTestAPI();
-  //   getCoordinate();
-  // }, []);
-
-  const getWeather = async () => {
+  const setLocation = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
-    if (!granted) {
-      setOk(false);
-      const latitude = 37.511336;
-      const longitude = 127.086262;
-      const location = await Location.reverseGeocodeAsync(
-        { latitude, longitude },
-        { useGoogleMaps: false }
-      );
-      setCity(location[0].city);
-
-      const response = await fetch(
-        `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&lang=kr&exclude=alerts&appid=${API_KEY}&units=metric`
-      ).catch((error) => {
-        console.log(error);
-      });
-      const json = await response.json();
-      setDays(json.daily);
-      // console.log(latitude, longitude);
-      // console.log(typeof latitude, typeof longitude);
-    } else {
+    if (granted) {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync({ accuracy: 5 });
@@ -79,85 +49,66 @@ export default function Weather() {
         { latitude, longitude },
         { useGoogleMaps: false }
       );
-      setCity(location[0].city);
+      // setCity(location[0].city);
+      console.log("done");
+      createWeatherAPI(latitude, longitude, location[0].city);
+    }
+    weatherAPI();
+  };
 
-      const response = await fetch(
-        `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&lang=kr&exclude=alerts&appid=${API_KEY}&units=metric`
-      ).catch((error) => {
-        console.log(error);
-      });
-      const json = await response.json();
-      setDays(json.daily);
-      // createWeatherAPI(latitude, longitude);
-      // console.log(latitude, longitude);
-      // console.log(typeof latitude, typeof longitude);
+  // useEffect(() => {
+  //   // AsyncStorage.clear(); // 사용금지
+  //   idTestAPI();
+  //   getCoordinate();
+  // }, []); ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
+
+  useEffect(() => {
+    weatherAPI();
+    // AsyncStorage.clear(); // 사용금지
+    idTestAPI();
+  }, []);
+
+  const reloadWeather = () => {
+    setLocation();
+  };
+
+  const createWeatherAPI = async (latitude, longitude, city) => {
+    const res = await axios.post(BASE_IP + "/weather/createWeather", {
+      appId: appId.current,
+      latitude,
+      longitude,
+      city,
+    });
+    console.log("createWeather: ", res.data.status);
+  };
+
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem("@storage_Id", value);
+    } catch (e) {
+      // saving error
     }
   };
 
-  const reloadWeather = () => {
-    getWeather();
+  const idTestAPI = async () => {
+    const value = await AsyncStorage.getItem("@storage_Id");
+
+    console.log("value", value);
+    if (value == null) {
+      const id = Date.now().toString();
+
+      appId.current = id;
+      const response = await axios.post(BASE_IP + "/user/addUser", {
+        macId: "E4:5F:01:D6:0F:91",
+        appId: id,
+      });
+      console.log("idTest : ", response.data.status);
+      storeData(id);
+    } else {
+      console.log("appId is OK");
+      appId.current = value;
+    }
   };
-
-  useEffect(() => {
-    getWeather();
-  }, []);
-
-  // const getWeatherAPI = async () => {
-  //   const res = await axios.get(
-  //     "http://172.30.1.72:8080/weather/get-coordinate"
-  //   );
-  //   console.log(res.data);
-  //   return res;
-  // };
-  // const createWeatherAPI = async (latitude, longitude) => {
-  //   const res = await axios.post(
-  //     "http://172.30.1.72:8080/weather/createWeather",
-  //     {
-  //       appId: appId.current,
-  //       latitude,
-  //       longitude,
-  //     }
-  //   );
-  //   // console.log(res.data);
-  // };
-
-  // const testAPI = async () => {
-  //   const res = await axios.get("http://172.30.1.72:8080/user/default");
-  //   console.log(res.data);
-  // };
-
-  // // const [appId, setAppId] = useState("");
-  // const appId = useRef("");
-
-  // const idTestAPI = async () => {
-  //   const value = await AsyncStorage.getItem("@storage_Id");
-
-  //   console.log("value", value);
-
-  //   if (value == null) {
-  //     const id = Date.now().toString();
-  //     storeData(id);
-  //     appId.current = id;
-  //     const response = await axios.post(
-  //       "http://172.30.1.72:8080/user/addUser",
-  //       {
-  //         macId: "E4:5F:01:D6:0F:91",
-  //         appId: id,
-  //       }
-  //     );
-  //   } else appId.current = value;
-  // };
-
-  // 첫 실행시 Id 값 조회, 없으면 => date 저장하게하고, 있으면 => main으로 value를 props로 전달시켜주
-  // 있으면 서버랑 통신할 Id로 사용.
-
-  // const storeData = async (value) => {
-  //   try {
-  //     await AsyncStorage.setItem("@storage_Id", value);
-  //   } catch (e) {
-  //     // saving error
-  //   }
-  // };
 
   return (
     <View>
@@ -165,7 +116,7 @@ export default function Weather() {
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
           <Text
             style={styles.title}
-            onPress={() => navigation.navigate("WeatherDetails")}
+            onPress={() => AsyncStorage.clear()} // 사용금지
           >
             오늘 날씨
           </Text>
@@ -177,17 +128,17 @@ export default function Weather() {
           />
         </View>
 
-        {days?.length === 0 ? (
-          <View>
-            <ActivityIndicator
-              color="#A3C1C6"
-              size="small"
-              style={{ marginTop: 30 }}
-            />
-          </View>
-        ) : (
+        {days?.length === 0 ? null : (
           <View>
             <View style={styles.container}>
+              {/* <View>
+                <ActivityIndicator
+                  animating={loading}
+                  color="#A3C1C6"
+                  size="small"
+                  style={{ marginTop: 30 }}
+                />
+              </View> */}
               <View style={styles.flexbox1}>
                 <Fontisto
                   name={icons[days[0].weather[0].main]}
@@ -283,3 +234,24 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
 });
+
+// const getCoordinate = async () => {
+//   const res = await getWeatherAPI();
+//   console.log(res.data);
+// };
+
+// const getWeatherAPI = async () => {
+//   const res = await axios.get(
+//     "http://172.30.1.72:8080/weather/get-coordinate"
+//   );
+//   console.log(res.data);
+//   return res;
+// };
+
+// const testAPI = async () => {
+//   const res = await axios.get("http://172.30.1.72:8080/user/default");
+//   console.log(res.data);
+// };
+
+// 첫 실행시 Id 값 조회, 없으면 => date 저장하게하고, 있으면 => main으로 value를 props로 전달시켜주
+// 있으면 서버랑 통신할 Id로 사용.
