8324eb5b3c81358e73e095103224eafd9c0df098
leehh6024 <dlgusghchlrh@gmail.com>
2022-12-09 11:58:30 +0900

    Modify: Kewword

--- app/pages/Settings/Tab.js
+++ app/pages/Settings/Tab.js
@@ -13,27 +13,33 @@ export default function Tab({ activeTab, setActiveTab }) {
           일정
         </Text>
       </View>
-      <Text
-        style={styles(activeTab).categoryText2}
-        isActive={activeTab === "W"}
-        onPress={() => setActiveTab("W")}
-      >
-        날씨
-      </Text>
-      <Text
-        style={styles(activeTab).categoryText3}
-        isActive={activeTab === "B"}
-        onPress={() => setActiveTab("B")}
-      >
-        버스
-      </Text>
-      <Text
-        style={styles(activeTab).categoryText4}
-        isActive={activeTab === "N"}
-        onPress={() => setActiveTab("N")}
-      >
-        뉴스
-      </Text>
+      <View style={styles(activeTab).categoryText22}>
+        <Text
+          style={styles(activeTab).categoryText2}
+          isActive={activeTab === "W"}
+          onPress={() => setActiveTab("W")}
+        >
+          날씨
+        </Text>
+      </View>
+      <View style={styles(activeTab).categoryText33}>
+        <Text
+          style={styles(activeTab).categoryText3}
+          isActive={activeTab === "B"}
+          onPress={() => setActiveTab("B")}
+        >
+          버스
+        </Text>
+      </View>
+      <View style={styles(activeTab).categoryText44}>
+        <Text
+          style={styles(activeTab).categoryText4}
+          isActive={activeTab === "N"}
+          onPress={() => setActiveTab("N")}
+        >
+          뉴스
+        </Text>
+      </View>
     </View>
   );
 }
@@ -43,7 +49,8 @@ const styles = (activeTab) =>
     categoryText1: {
       justifyContent: "center",
       alignItems: "center",
-      marginBottom: 30,
+      marginBottom: 15,
+      paddingTop: 15,
       fontSize: 18,
       color: "#666666",
       fontWeight: "700",
@@ -51,29 +58,68 @@ const styles = (activeTab) =>
     categoryText11: {
       justifyContent: "center",
       alignItems: "center",
+      marginBottom: 10,
+      width: 50,
       borderWidth: activeTab === "C" ? 1 : null,
-      backgroundColor: activeTab === "C" ? "blue" : "red",
-      borderRadius: activeTab === "C" ? "15" : null,
+      borderColor: activeTab === "C" ? "#e2e2e2" : null,
+      borderRadius: activeTab === "C" ? "50" : null,
+      backgroundColor: activeTab === "C" ? "#e2e2e2" : null,
     },
     categoryText2: {
-      marginBottom: 30,
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 15,
+      paddingTop: 15,
       fontSize: 18,
       color: "#666666",
       fontWeight: "700",
-      backgroundColor: activeTab === "W" ? "blue" : null,
+    },
+    categoryText22: {
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 10,
+      width: 50,
+      borderWidth: activeTab === "W" ? 1 : null,
+      borderColor: activeTab === "W" ? "#e2e2e2" : null,
+      borderRadius: activeTab === "W" ? "50" : null,
+      backgroundColor: activeTab === "W" ? "#e2e2e2" : null,
     },
     categoryText3: {
-      marginBottom: 30,
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 15,
+      paddingTop: 15,
       fontSize: 18,
       color: "#666666",
       fontWeight: "700",
-      backgroundColor: activeTab === "B" ? "blue" : null,
+    },
+    categoryText33: {
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 10,
+      width: 50,
+      borderWidth: activeTab === "B" ? 1 : null,
+      borderColor: activeTab === "B" ? "#e2e2e2" : null,
+      borderRadius: activeTab === "B" ? "50" : null,
+      backgroundColor: activeTab === "B" ? "#e2e2e2" : null,
     },
     categoryText4: {
-      marginBottom: 30,
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 15,
+      paddingTop: 15,
       fontSize: 18,
       color: "#666666",
       fontWeight: "700",
-      backgroundColor: activeTab === "N" ? "blue" : null,
+    },
+    categoryText44: {
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 10,
+      width: 50,
+      borderWidth: activeTab === "N" ? 1 : null,
+      borderColor: activeTab === "N" ? "#e2e2e2" : null,
+      borderRadius: activeTab === "N" ? "50" : null,
+      backgroundColor: activeTab === "N" ? "#e2e2e2" : null,
     },
   });
