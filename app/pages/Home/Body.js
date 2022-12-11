c01d36842100b08c37ce32208f465204230196e6
leehh6024 <dlgusghchlrh@gmail.com>
2022-12-11 17:28:24 +0900

    Modify: Body Component

--- app/pages/Home/Body.js
+++ app/pages/Home/Body.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { ScrollView, Dimensions } from "react-native";
+import { ScrollView } from "react-native";
 import Example from "./components/Example.js";
 import Calendar from "./components/Calendar.js";
 import Weather from "./components/Weather.js";
 import Bus from "./components/Bus.js";
 import News from "./components/News.js";
-
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function Body() {
   return (
